0eb12c2b58ca71088953c0ce9f8385c5ff1440f6
adrianmllorca-source <adrianm.llorca@gmail.com>
2026-08-24 03:41:33 +0800

    added timer

--- scripts/qte_manager.gd
+++ scripts/qte_manager.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var qte_ui: Control = $"CanvasLayer/QTE UI"
+@onready var timer: Timer = $Timer
 @onready var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 @onready var letter_prompt_dict: Dictionary
 @onready var success_count: int = 0
