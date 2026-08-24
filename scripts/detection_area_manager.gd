f3a5c436457dedf1eadd8245235b0c5ffbd77d1f
adrianmllorca-source <adrianm.llorca@gmail.com>
2026-08-24 21:51:42 +0800

    implemented player detecting guard

--- scripts/detection_area_manager.gd
+++ scripts/detection_area_manager.gd
@@ -1,12 +1,17 @@
 extends Node2D
 
 @export var player: CharacterBody2D
-@export var detection_area: CollisionShape2D
+@export var detection_area: Area2D
 
 @onready var r_pos: Marker2D = $RPos
 @onready var l_pos: Marker2D = $LPos
 @onready var d_pos: Marker2D = $DPos
 @onready var u_pos: Marker2D = $UPos
+
+func _ready() -> void:
+	if detection_area:
+		detection_area.body_entered.connect(on_guard_detected)
+		detection_area.body_exited.connect(on_guard_undetected)
 
 func _physics_process(delta: float) -> void:
 	if player.last_dir_pressed and detection_area:
@@ -19,3 +24,11 @@ func _physics_process(delta: float) -> void:
 				detection_area.position = d_pos.position
 			"D":
 				detection_area.position = r_pos.position
+
+func on_guard_detected(guard):
+	if guard is Guard:
+		print("guard detected")
+
+func on_guard_undetected(guard):
+	if guard is Guard:
+		print("guard undetected")
