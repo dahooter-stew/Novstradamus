761266c3c26df6f6969b784372e2f25b745cf45c
adrianmllorca-source <adrianm.llorca@gmail.com>
2026-08-26 03:21:53 +0800

    added basement tileset

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,8 +3,9 @@ extends Node2D
 @onready var player: Player = $Player
 @onready var qte_manager: Node2D = $"QTE Manager"
 @onready var menus_manager: MenusManager = $"Menus Manager"
+@onready var level_manager: LevelManager = $LevelManager
 
-var guard_list: Array
+#var guard_list: Array
 
 func _ready() -> void:
 	#guard_list.append(find_children("*", "Guard"))
