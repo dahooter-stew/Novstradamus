1741ac52f9c11cdab2cc42a0ea798047d13b67b9
adrianmllorca-source <adrianm.llorca@gmail.com>
2026-08-26 16:57:00 +0800

    implemented guard following path

--- scenes/levels/guard_path_follow.gd
+++ scenes/levels/guard_path_follow.gd
@@ -2,3 +2,6 @@ class_name GuardPathFollow extends PathFollow2D
 
 @export var pathing_guard: Guard
 @export var guard_speed: float
+
+func _physics_process(delta: float) -> void:
+	progress_ratio += (guard_speed / 100) * delta
