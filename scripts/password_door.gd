1e925f7d87ea2c4234e547e15750a7f79968a1b7
adrianmllorca-source <adrianm.llorca@gmail.com>
2026-08-29 03:24:02 +0800

    implemented prompting password door interaction

--- scripts/password_door.gd
+++ scripts/password_door.gd
@@ -1,3 +1,6 @@
-class_name PasswordDoor extends Area2D
+class_name PasswordDoor extends StaticBody2D
 
 @export var password_manager: PasswordManager
+@export var password_prompt: Control
+
+@onready var is_in_player_detection_area: bool = false
--- scripts/password_manager.gd
+++ scripts/password_manager.gd
@@ -1,26 +1,35 @@
 class_name PasswordManager extends Node
 
-@export var door: Area2D
+@export var door: PasswordDoor
 @export var ui: Control
+
+@onready var attempts_remaining: int = 3
 
 var password_sequence: Array
 
 func _ready() -> void:
 	ui.keypad_button_pressed.connect(on_keypad_button_pressed)
 	
-	start_sequence()
+	#start_sequence()
 
-func generate_password():
+func _process(delta: float) -> void:
+	pass
+
+func generate_password_sequence():
+	var generated_sequence: Array
 	for i in 5:
-		password_sequence.append(randi_range(0, 8))
+		generated_sequence.append(randi_range(0, 8))
+	return generated_sequence
 
 func start_sequence():
 	ui.show()
-	await get_tree().create_timer(0.5).timeout
+	await get_tree().create_timer(0.7).timeout
 	
 func indicate_password_sequence():
 	for i in password_sequence:
 		await ui.indicate_keypad_button(i)
 	
+
+
 func on_keypad_button_pressed(keypad_button):
 	print(keypad_button)
