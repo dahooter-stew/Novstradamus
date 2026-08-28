8fd7cf7c677ee321ac17c34dcb05cbde59582472
adrianmllorca-source <adrianm.llorca@gmail.com>
2026-08-29 02:31:20 +0800

    added password sequence activation logic
    added keypad enter button to submit sequence attempt

--- scripts/password_manager.gd
+++ scripts/password_manager.gd
@@ -7,6 +7,20 @@ var password_sequence: Array
 
 func _ready() -> void:
 	ui.keypad_button_pressed.connect(on_keypad_button_pressed)
+	
+	start_sequence()
 
+func generate_password():
+	for i in 5:
+		password_sequence.append(randi_range(0, 8))
+
+func start_sequence():
+	ui.show()
+	await get_tree().create_timer(0.5).timeout
+	
+func indicate_password_sequence():
+	for i in password_sequence:
+		await ui.indicate_keypad_button(i)
+	
 func on_keypad_button_pressed(keypad_button):
 	print(keypad_button)
