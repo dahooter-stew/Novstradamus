3087de6e33ba4bea9ef5538c7e3e00064f1db900
adrianmllorca-source <adrianm.llorca@gmail.com>
2026-09-03 01:26:43 +0800

    declared scene transition animation manager class

---
+++ scenes/scene_transition_manager.gd
@@ -0,0 +1,21 @@
+class_name SceneTransitionAnimationManager
+extends Node
+
+@onready var black_rect: ColorRect = $CanvasLayer/ColorRect
+var scene_transition_anim_tween: Tween
+
+func play_scene_fade_out():
+	black_rect.modulate.a = 0
+	if scene_transition_anim_tween:
+		scene_transition_anim_tween.kill()
+	
+	scene_transition_anim_tween = create_tween().set_trans(Tween.TRANS_CUBIC)
+	scene_transition_anim_tween.tween_property(black_rect, "modulate:a", 1, 1)
+	
+func play_scene_fade_in():
+	black_rect.modulate.a = 0
+	if scene_transition_anim_tween:
+		scene_transition_anim_tween.kill()
+	
+	scene_transition_anim_tween = create_tween().set_trans(Tween.TRANS_CUBIC)
+	scene_transition_anim_tween.tween_property(black_rect, "modulate:a", 0, 1)
