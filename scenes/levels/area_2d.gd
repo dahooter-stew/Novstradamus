cce7605f7ba1055ff7f9854ec43603b274853d69
adrianmllorca-source <adrianm.llorca@gmail.com>
2026-09-04 02:41:06 +0800

    added guard spritesheet

--- scenes/levels/area_2d.gd
+++ scenes/levels/area_2d.gd
@@ -7,10 +7,10 @@ func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
-	collision_shape.set_deferred("disabled", true)
 	if body is Player:
+		collision_shape.set_deferred("disabled", true)
 		var guard_instance = preload("res://scenes/guard.tscn").instantiate()
 		guard_instance.global_position = marker.global_position
 		guard_instance.sight_rotation = -180
-		#guard_instance.velocity.x = -100
+		guard_instance.velocity.x = -100
 		get_parent().add_child(guard_instance)
